4784c2bda28c5a9b378de4ad23b5249e6d029642
mehakx <mehak.garg037@gmail.com>
2025-12-03 17:27:54 -0500

    Delete websocket-demo directory

--- websocket-demo/client/main.jsx
+++
@@ -1,7 +0,0 @@
-import { createRoot } from "react-dom/client";
-import { App } from "./App";
-
-window.addEventListener("load", () => {
-  let root = createRoot(document.getElementById("root"));
-  root.render(<App />);
-});
